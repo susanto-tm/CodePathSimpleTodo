fb17ea308a8d43d62bfa92922d1fa6013364f5af
susanto-tm <susanto.tm@gmail.com>
2020-11-15 00:50:35 -0500

    Added functionality to add descriptions from within the main activity through a toggle and accounted for incorrect input shown through a Toast text.

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -1,16 +1,25 @@
 package com.example.simpletodo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class EditActivity extends AppCompatActivity {
 
+    private String editItemState;
+    private String editDescState;
+
     EditText editItem;
-    Button saveButton;
+    EditText editDesc;
+    Button saveButtonItem;
+    Button saveButtonDesc;
+    Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,27 +27,60 @@ public class EditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit);
 
         editItem = findViewById(R.id.editItemText);
-        saveButton = findViewById(R.id.saveButton);
+        editDesc = findViewById(R.id.editDesc);
+        saveButtonItem = findViewById(R.id.saveButton);
+        saveButtonDesc = findViewById(R.id.saveButtonDesc);
+        doneButton = findViewById(R.id.doneButton);
 
         getSupportActionBar().setTitle("Edit Item");
 
+        // Initialize edit screen
+        editItemState = getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT);
+        editDescState = getIntent().getStringExtra(MainActivity.KEY_ITEM_DESC);
         editItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+        editDesc.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_DESC));
 
-        saveButton.setOnClickListener(new View.OnClickListener(){
+        saveButtonItem.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Override value if changed
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                editItemState = editItem.getText().toString();
+                Toast.makeText(getApplicationContext(), "Todo Item Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        saveButtonDesc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Override value if changed
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                editItemState = editItem.getText().toString();
+                editDescState = editDesc.getText().toString();
+                Toast.makeText(getApplicationContext(), "Todo Description Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create intent that contains results of modified data
                 Intent intent = new Intent();
 
                 // Pass data (results of editing)
-                intent.putExtra(MainActivity.KEY_ITEM_TEXT, editItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, editItemState);
+                intent.putExtra(MainActivity.KEY_ITEM_DESC, editDescState);
                 intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
 
                 // Set the result of the intent
                 setResult(RESULT_OK, intent);
+
                 // Finish activity, close screen and go back
                 finish();
             }
         });
+
     }
 }
--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.simpletodo;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,15 +20,19 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static final String KEY_ITEM_TEXT = "item_text";
+    public static final String KEY_ITEM_DESC = "item_desc";
     public static final String KEY_ITEM_POSITION = "item_position";
     public static final int EDIT_TEXT_CODE = 20;
+    private String itemTodoState = "";
+    private String itemDescState = "";
 
     List<String> todoItems;
+    List<String> todoDescription;
     Button addButton;
     EditText editText;
     RecyclerView recyclerView;
     ItemsAdapter itemsAdapter;
-
+    Switch descriptionToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +42,17 @@ public class MainActivity extends AppCompatActivity {
         addButton = findViewById(R.id.addButton);
         editText = findViewById(R.id.editText);
         recyclerView = findViewById(R.id.recyclerView);
+        descriptionToggle = findViewById(R.id.descriptionToggle);
 
         loadItems();
 
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
             @Override
             public void onItemLongClicked(int position) {
-                // Delete item from model
+                // Delete item and description from model
                 todoItems.remove(position);
+                todoDescription.remove(position);
+
                 // Notify adapter the position we performed the delete
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
@@ -59,14 +64,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClicked(int position) {
                 Log.d("MainActivity", "Single click at position " + position);
+
                 // Create new activity
                 Intent i = new Intent(MainActivity.this, EditActivity.class);
+
                 // Pass data being edited
                 i.putExtra(KEY_ITEM_TEXT, todoItems.get(position));
+                i.putExtra(KEY_ITEM_DESC, todoDescription.get(position));
                 i.putExtra(KEY_ITEM_POSITION, position);
+
                 // Display activity
                 startActivityForResult(i, EDIT_TEXT_CODE);
-
             }
         };
 
@@ -77,16 +85,60 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String todoItem = editText.getText().toString();
-                // Add Item to model
-                todoItems.add(todoItem);
-                // Notify adapter an item is inserted
-                itemsAdapter.notifyItemInserted(todoItems.size() - 1);
-                editText.setText("");
-                Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
-                saveItems();
+                if (!descriptionToggle.isChecked() && "".equals(itemTodoState)) {
+                    itemTodoState = editText.getText().toString();
+                }
+                if (descriptionToggle.isChecked() && "".equals(itemDescState)) {
+                    itemDescState = editText.getText().toString();
+                }
+
+                if (!"".equals(itemTodoState) && itemTodoState != null) {
+                    // Add Item to model
+                    todoItems.add(itemTodoState);
+
+                    // Add description to model
+                    todoDescription.add(itemDescState);
+                    Log.e("MainActivity", "Descriptions: " + todoDescription.toString());
+
+                    // Notify adapter an item is inserted
+                    itemsAdapter.notifyItemInserted(todoItems.size() - 1);
+                    descriptionToggle.setChecked(false);  // reset switch
+                    editText.setText("");  // reset edit text
+                    Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+                    saveItems();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Please enter item", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+        descriptionToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    itemTodoState = editText.getText().toString();
+                    if (itemDescState.equals("")) {
+                        editText.setText("");
+                    }
+                    else {
+                        editText.setText(itemDescState);
+                    }
+                    Log.e("MainActivity", "ItemTodoState: " + itemTodoState);
+                }
+                else {
+                    itemDescState = editText.getText().toString();
+                    if (itemTodoState.equals("")) {
+                        editText.setText("");
+                    }
+                    else {
+                        editText.setText(itemTodoState);
+                    }
+                    Log.e("MainActivity", "ItemDescState: "  + itemDescState);
+                }
+            }
+        });
+
     }
 
     @Override
@@ -94,11 +146,13 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 20 && resultCode == RESULT_OK) {
             // Retrieve the updated text value
             String itemText = data.getStringExtra(KEY_ITEM_TEXT);
+            String itemDesc = data.getStringExtra(KEY_ITEM_DESC);
             // Extract original position of edited item from the position key
             int position = data.getExtras().getInt(KEY_ITEM_POSITION);
 
             // Update the model at the right position with the new text
             todoItems.set(position, itemText);
+            todoDescription.set(position, itemDesc);
 
             // Notify the adapter
             itemsAdapter.notifyItemChanged(position);
@@ -112,23 +166,30 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private File getDataFile() {
-        return new File(getFilesDir(), "data.txt");
+    private File getDataFileItems() {
+        return new File(getFilesDir(), "dataItems.txt");
+    }
+
+    private File getDataFileDescription() {
+        return new File(getFilesDir(), "dataDescription.txt");
     }
 
     // Load items by reading every line of the data file
     private void loadItems() {
         try {
-            todoItems = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+            todoItems = new ArrayList<>(FileUtils.readLines(getDataFileItems(), Charset.defaultCharset()));
+            todoDescription = new ArrayList<>(FileUtils.readLines(getDataFileDescription(), Charset.defaultCharset()));
         } catch (IOException e) {
             Log.e("MainActivity", "Error reading items", e);
             todoItems = new ArrayList<>();
+            todoDescription = new ArrayList<>();
         }
     }
     // Saves items by writing them into the data file
     private void saveItems() {
         try {
-            FileUtils.writeLines(getDataFile(), todoItems);
+            FileUtils.writeLines(getDataFileItems(), todoItems);
+            FileUtils.writeLines(getDataFileDescription(), todoDescription);
         } catch (IOException e) {
             Log.e("MainActivity", "Error writing items", e);
         }
